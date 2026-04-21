dcd5a0c5deaabbdb34bae24963a08ff8da15cfa7
RealRottale1 <camarkham2@gmail.com>
2026-04-21 08:16:27 -0400

    U

---
+++ test_gradebook.py
@@ -0,0 +1,12 @@
+from gradebook import letterGrade
+
+@pytest.mark.parametrize("grade, expected", [
+    (90, "A"),
+    (80, "B"),
+    (70, "C"),
+    (60, "D"),
+    (50, "F")
+])
+
+def test_ticket_tiers(grade, expected):
+    assert letterGrade(code) == expected
